0489f5060a7cc28e1b9dfdde89c53a327282c854
Kurt Madel <kmadel@mac.com>
2015-06-05 11:07:24 -0400

    Update flow.groovy

--- flow.groovy
+++ flow.groovy
@@ -1,11 +1,20 @@
 stage 'build image'
 node('docker') {
 	docker.withServer('tcp://127.0.0.1:1234'){
-	  docker.withRegistry('https://registry.hub.docker.com/', 'docker-registry-kmadel-login') {	
-	  	git url: 'https://github.com/kmadel/jenkins-slave.git'
-	  	def jenkinsSlaveImage = docker.build "kmadel/jenkins-slave:latest"
-	  	stage 'push image'
-	  	jenkinsSlaveImage.push 'latest'
+	  docker.withRegistry('https://registry.hub.docker.com/', 'docker-registry-kmadel-login') {
+	  	
+	  	parallel(jenkinsSlaveBuildPush: {
+	  		git url: 'https://github.com/kmadel/jenkins-slave.git'
+	  		def jenkinsSlaveImage = docker.build "kmadel/jenkins-slave:latest"
+	  		stage 'push kmadel/jenkins-slave image'
+	  		jenkinsSlaveImage.push 'latest'
+    		}, jenkinsEnterpriseBuildPush: {
+    			stage 'build kmadel/jenkins-enterprise image'
+	  		git url: 'https://github.com/kmadel/jenkins-enterprise.git'
+	  		def jenkinsSlaveImage = docker.build "kmadel/jenkins-enterprise:latest"
+	  		stage 'push kmadel/jenkins-enterprise image'
+	  		jenkinsSlaveImage.push 'latest'
+    		})
 	  }
 	}
 }
